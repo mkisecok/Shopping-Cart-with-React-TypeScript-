2362b2d1a1428a00893305b332ccec95a42b55f1
Mkisecok <mahmutkisecok@gmail.com>
2022-01-20 13:42:43 +0100

    feat: Add Product folder

--- react-shopping-cart/src/App.tsx
+++ react-shopping-cart/src/App.tsx
@@ -1,8 +1,9 @@
 import { useState } from 'react';
 import { useQuery } from 'react-query';
 //component
-import { Drawer, LinearProgress, Grid, Badge } from '@material-ui/core';
+import { Drawer, Grid, Badge, CircularProgress } from '@material-ui/core';
 import { AddShoppingCart } from '@material-ui/icons';
+import Product from './Product/Prduct';
 //styles
 import { Wrapper } from './App.styles';
 import './App.css';
@@ -15,15 +16,29 @@ export type CartItemType={
   price:number;
   title:string;
   amount:number;
+  rating:number;
 
 }
 
-const getProducts = async()=>{
+const getProducts = async(): Promise <CartItemType[]> =>
   await (await fetch('https://fakestoreapi.com/products')).json();
-}
+
 
 
 function App() {
+  const { data, isLoading, error }=useQuery<CartItemType[]>('products', getProducts );
+  console.log(data);
+
+  const getTotalItems = () => null;
+
+  const handleAddToCart = (clickedProduct:CartItemType) => null;
+
+  const handleRemoveFromCart = () => null;
+
+  if(isLoading) return < CircularProgress  />
+
+  if(error) return <div> Something went wrong ..</div>
+  
   return (
     <div className="App">
     Start
---
+++ react-shopping-cart/src/Product/Prduct.tsx
@@ -0,0 +1,25 @@
+import { Button } from "@material-ui/core";
+//Types
+import { CartItemType } from "../App";
+
+//styles
+import { Wrapper } from "./Product.styles";
+
+type Props={
+    product:CartItemType;
+    handleAddToCart:(clickedProduct: CartItemType)=> void;
+}
+const Product: React.FC<Props> = ({product, handleAddToCart}) =>(
+    <Wrapper>
+        <img src={product.image} alt={product.title}/>
+        <div>
+            <h3>{product.title}</h3>
+            <p>{product.description}</p>
+            <h4>€{product.price}</h4>
+            <p>{product.rating}</p>
+
+        </div>
+        <Button onClick={()=>handleAddToCart(product) } >Add to Cart</Button>
+    </Wrapper>
+);
+export default Product
